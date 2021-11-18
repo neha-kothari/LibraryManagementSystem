dd085afc9b0aab23bb73ddb0330b5b579a26d253
lumos-ap <ankita008paul@gmail.com>
2021-11-18 12:59:28 +0530

    SignUp Page layout

--- src/main/webapp/lms_ui/src/App.js
+++ src/main/webapp/lms_ui/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import './App.css';
 import SignIn from "./Component/SignIn";
 import SignUp from "./Component/SignUp";
+import SignUpContainer from "./Component/SignUpContainer"
 
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
             <Route exact path="/" element={<SignIn/>}/>
             <Route exact path="/SignIn" element={<SignIn/>}/>
             <Route exact path="/SignUp" element={<SignUp/>}/>
+            <Route exact path="/s" element={<SignUpContainer/>}/>
             {/*<Route path="*" element={<NotFound/>}/>*/}
           </Routes>
         </Router>
