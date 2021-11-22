72ff5cefffe4345f9075173a16953f7f30f4f471
lumos-ap <ankita008paul@gmail.com>
2021-11-22 11:35:47 +0530

    App.js components removed.

--- src/main/webapp/lms_ui/src/App.js
+++ src/main/webapp/lms_ui/src/App.js
@@ -3,9 +3,7 @@ import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import './App.css';
 import SignIn from "./Component/SignIn";
 import SignUp from "./Component/SignUp";
-import LibDashboard from "./Component/LibDashboard"
-import SignUpContainer from "./Component/SignUpContainer"
-
+import LibDashboard from "./Component/LibDashboard";
 
 class App extends Component {
   render() {
@@ -16,6 +14,7 @@ class App extends Component {
             <Route exact path="/SignIn" element={<SignIn/>}/>
             <Route exact path="/SignUp" element={<SignUp/>}/>
             <Route exact path="/LibraryDashboard" element={<LibDashboard/>}/>
+            <Route exact path="/SignUp" element={<SignUp/>}/>
             {/*<Route path="*" element={<NotFound/>}/>*/}
           </Routes>
         </Router>
