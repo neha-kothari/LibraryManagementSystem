d7913f5e195e996a5c89df1394d2c9e07c2ef2f9
shubhi2206 <maheshwarishubhi22@gmail.com>
2021-11-23 12:39:09 +0530

    Register Book Page

--- src/main/webapp/lms_ui/src/App.js
+++ src/main/webapp/lms_ui/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import SignIn from "./Component/SignIn";
 import SignUp from "./Component/SignUp";
 import LibDashboard from "./Component/LibDashboard";
+import RegisterBook from "./Component/RegisterBook";
+import Basic from "./Component/Basic"
 
 class App extends Component {
   render() {
@@ -14,7 +16,10 @@ class App extends Component {
             <Route exact path="/SignIn" element={<SignIn/>}/>
             <Route exact path="/SignUp" element={<SignUp/>}/>
             <Route exact path="/LibraryDashboard" element={<LibDashboard/>}/>
+              <Route exact path="/RegisterBook" element={<RegisterBook/>}/>
             <Route exact path="/SignUp" element={<SignUp/>}/>
+              {/*<Route exact path="/Basic" element={<Basic/>}/>*/}
+
             {/*<Route path="*" element={<NotFound/>}/>*/}
           </Routes>
         </Router>
