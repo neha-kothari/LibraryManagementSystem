95d95e846b57d996feee4a5b0dd8fe29003ab536
yrus <shahrushi199891@gmail.com>
2021-12-12 10:37:54 +0530

    Fix for Get All Students

--- src/main/java/com/iiitb/lms/repositories/UserRepository.java
+++ src/main/java/com/iiitb/lms/repositories/UserRepository.java
@@ -12,6 +12,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmailAddress(String email);
     User findByUserId(int user_id);
 
-    @Query(value = "SELECT * FROM user u WHERE user_type = 2", nativeQuery = true)
+    @Query(value = "SELECT u.*,m.*, 1 as clazz_ FROM user u LEFT OUTER JOIN member m on u.user_id=m.user_id WHERE u.user_type = 2", nativeQuery = true)
     List<User> getStudents();
 }
